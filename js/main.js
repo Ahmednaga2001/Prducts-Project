6c33aaf9ad6bc491abd1b935c7a5f977ad932f6a
Ahmed Naga <ahmednaga02001@gmail.com>
2024-02-01 20:59:11 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function scroller() {
+    let scroller = document.querySelector(".scroller");
+    let height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    window.addEventListener("scroll", () => {
+      let scrollTop = document.documentElement.scrollTop;
+      scroller.style.width = `${(scrollTop / height) * 100}%`;
+    });
+  }
+  
+scroller()  
