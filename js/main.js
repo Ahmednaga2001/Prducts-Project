02a6b0f84df50a34c9ca232258e87dc695a40bae
Ahmed-Ebalouty <ahmedelbalouty01@gmail.com>
2024-02-05 18:45:32 +0200

    Edit commit

--- js/main.js
+++ js/main.js
@@ -38,3 +38,20 @@ async function initMap() {
 }
 
 initMap();
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+
